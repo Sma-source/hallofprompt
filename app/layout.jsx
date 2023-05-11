a8c99eb27853692f33a94a5c6591666d9fdccc5a
Sma-source <rabhi.smain@gmail.com>
2023-05-11 17:34:16 +0200

    create layout.jsx, add metadata en main layout

---
+++ app/layout.jsx
@@ -0,0 +1,21 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Hall Of Prompt",
+  description: "The best Midjourney Prompts to inspire you!",
+};
+
+const BaseLayout = () => {
+  return (
+    <html lang="fr">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default BaseLayout;
