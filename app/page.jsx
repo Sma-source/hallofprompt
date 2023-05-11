55a8835d4c9b2811687e30e6c0cec6cf0845c9c7
Sma-source <rabhi.smain@gmail.com>
2023-05-11 19:04:24 +0200

    change text fr

--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,12 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
-        Take inspiration from
+        Inspire toi des Meilleures
         <br className="max-md:hidden" />
-        <span className="blue_gradient text-center">
-          The Best Midjourney Prompts!
-        </span>
+        <span className="blue_gradient text-center">Prompts Midjourney!</span>
       </h1>
       <p className="desc text-center">
-        Hall Of prompt is an open-source AI prompting tool for modern world to
+        Hall Of Prompt is an open-source AI prompting tool for modern world to
         discover, create and share creative prompts.
       </p>
     </section>
