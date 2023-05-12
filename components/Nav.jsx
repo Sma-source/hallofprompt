f9512a830266da44e0daff91f5fbc0ab07dfa5c9
Sma-source <rabhi.smain@gmail.com>
2023-05-12 15:20:10 +0200

    update Nav.jsx: add ui, nav link  and logo

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,7 +5,20 @@ import Image from "next/image";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image
+          src="/assets/images/logo.svg"
+          alt="logo"
+          width={30}
+          height={30}
+          className="object-contain"
+        />
+        <p className="logo_text">Hall Of Prompts</p>
+      </Link>
+    </nav>
+  );
 };
 
 export default Nav;
