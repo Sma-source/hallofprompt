04fd15dc29ec5c756964d13d68e2142038e65b01
Sma-source <rabhi.smain@gmail.com>
2023-05-12 15:04:52 +0200

    update Nav.jsx: make imports

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
   return <div>Nav</div>;
