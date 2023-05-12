1825c3d3125ad45edc043ffe04bcd98a56ed1dfa
Sma-source <rabhi.smain@gmail.com>
2023-05-12 16:16:55 +0200

    update Nav.jsx: add ui desktop nav if logged in

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -17,6 +18,33 @@ const Nav = () => {
         />
         <p className="logo_text">Hall Of Prompts</p>
       </Link>
+
+      {/* Desktop Navigation */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">
+              Crée un post
+            </Link>
+            <button type="button" onClick={signOut} className="outline_btn">
+              Déconnexion
+            </button>
+
+            <Link href="/profile">
+              <Image
+                title="Profile"
+                src="/assets/images/profile.png"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </nav>
   );
 };
