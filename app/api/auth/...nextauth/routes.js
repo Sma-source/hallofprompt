fd0749ab74ff237e8dfd00fbefff6486fd30dffa
Sma-source <rabhi.smain@gmail.com>
2023-05-14 00:06:45 +0200

    update routes.js

--- app/api/auth/...nextauth/routes.js
+++ app/api/auth/...nextauth/routes.js
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -10,11 +13,33 @@ const handler = NextAuth({
   ],
 
   callbacks: {
-    async session({ session }) {},
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email }); // getting the current user from the session
+      session.user.id = sessionUser._id.toString(); // store the user id from mongodb to session
+
+      return session;
+    },
 
     async signIn({ profile }) {
       try {
-      } catch (error) {}
+        await connectToDB();
+
+        //check if user already exists
+        const userExists = await User.findOne({ email: profile.email });
+
+        // if not, create a new document and save user in MongoDB
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.username.replace(" ", "").toLowerCase(), //make sur there is no space and make it lowercase
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log("Error checking if user exists: ", error.message);
+        return false;
+      }
     },
   },
 });
