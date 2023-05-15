63e1c1496cd8e10bb1833384b16822bb53924ac9
Sma-source <rabhi.smain@gmail.com>
2023-05-15 15:03:36 +0200

    make imports and render form component

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Form from "@components/Form";
 
 const CreatePrompt = () => {
-  return <div>CreatePrompt</div>;
+  return <Form />;
 };
 
 export default CreatePrompt;
