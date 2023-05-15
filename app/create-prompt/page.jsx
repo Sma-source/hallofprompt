c39d81decadb7d01dedd1d4d0b37e11126f6318f
Sma-source <rabhi.smain@gmail.com>
2023-05-15 16:25:07 +0200

    add ui form

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,15 +2,40 @@
 
 import { useState } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const [submitting, setIsSubmitting] = useState(false); // check if we are submiting the form
   const [post, setPost] = useState({ prompt: "", tag: "" }); // object value that we are posting
 
   const createPrompt = async (e) => {
-    e.preventDefault();
+    e.preventDefault(); //prevent reload browser
+    setIsSubmitting(true);
+
+    try {
+      //call api
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tage: post.tag,
+        }),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsSubmitting(false);
+    }
   };
   return (
     <Form
