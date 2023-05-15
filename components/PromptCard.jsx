77d4321f9991882622b4b02a53515e09bea5559f
Sma-source <rabhi.smain@gmail.com>
2023-05-15 19:21:23 +0200

    update PromptCard.jsx: passing props

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
-const PromptCard = () => {
+const PromptCard = ({ post }) => {
   return <div>PromptCard</div>;
 };
 
