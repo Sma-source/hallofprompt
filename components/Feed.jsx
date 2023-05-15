62f1b4df6ff7473adb3c3088759143471d657ca0
Sma-source <rabhi.smain@gmail.com>
2023-05-15 18:37:47 +0200

    update Feed.jsx: add ui

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
+import PromptCard from "./PromptCard";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+
+  const handleSearchChange = (e) => {};
+  return (
+    <section className="feed">
+      <form className="relative w-full flex-center">
+        <input
+          type="text"
+          placeholder="Recheche un tag ou un nom d'utilisateur"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+    </section>
+  );
 };
 
 export default Feed;
