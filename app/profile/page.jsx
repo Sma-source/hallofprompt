dabc5d531b4a97a62532f4c267e8b626c9f368d4
Sma-source <rabhi.smain@gmail.com>
2023-05-16 12:10:19 +0200

    update profile/page.jsx: render Profile component and pass props to it

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+"use client";
 
-const Profile = () => {
-  return <div>Profile</div>;
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+  const handleEdit = () => {};
+
+  const handleDelete = () => {};
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination"
+      data={[]}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
-export default Profile;
+export default MyProfile;
